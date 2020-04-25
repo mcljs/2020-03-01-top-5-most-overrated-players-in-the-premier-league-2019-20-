776c12927c83d3ccbab09493209b3c9ba5925378
mcljs <mcljs15@gmail.com>
2020-04-24 22:27:59 -0400

    regiones

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+  
+  ScrollReveal().reveal('.showcase');
+  ScrollReveal().reveal('.news-cards', { delay: 500 });   
+
+
